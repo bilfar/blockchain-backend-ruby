07ad38dfd405f47778230e81fc82ca5dfa5e3c1f
Fred <fredch87@outlook.com>
2018-08-22 09:35:20 +0100

    Minor changes to Sinatra app file

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 
 # Understands sending and receiving transaction hashes
 class BlockchainApp < Sinatra::Base
+  run! if app_file == $PROGRAM_NAME
 end
