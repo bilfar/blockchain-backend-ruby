b42af4bf45235e1ebf3defca3f4161b1394da8f4
Fred <fredch87@outlook.com>
2018-08-23 11:39:55 +0100

    Post route takes Json rather than form parameters

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'sinatra/base'
 require './lib/block'
 
 # Understands sending and receiving transaction hashes
 class BlockchainApp < Sinatra::Base
   post '/blocks/create' do
-    Block.create(params[:sender], params[:receiver], params[:value])
+    data = JSON.parse(request.body.read)['params']
+    Block.create(data['sender'], data['receiver'], data['value'])
   end
 
   run! if app_file == $PROGRAM_NAME
