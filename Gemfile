d879a4f0ab0237ad26624526570342b1a15fd97a
Fred <fredch87@outlook.com>
2018-08-23 09:30:18 +0100

    Blocks are hashed when added to db and linked via previous tx

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 ruby '2.5.0'
 
 gem 'digest'
+gem 'openssl'
 gem 'pg'
 gem 'rake'
 gem 'rubocop', '0.56.0'
--- lib/block.rb
+++ lib/block.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 require_relative './helpers.rb'
+require 'openssl'
 require 'pg'
 
 # Understands creating and retrieving new blocks
 class Block
   attr_reader :id
 
-  def initialize(id, sender, receiver, value, previous_tx)
+  def initialize(id, sender, receiver, value, previous_tx, hash)
     @id = id
     @sender = sender
     @receiver = receiver
     @value = value
     @previous_tx = previous_tx
-    @hash = get_hash
+    @hash = hash
   end
 
   def self.all
@@ -24,33 +25,35 @@ class Block
                 block['sender'],
                 block['receiver'],
                 block['value'],
-                block['previous_tx'])
+                block['previous_tx'],
+                block['hash'])
     end
   end
 
   def self.create(sender, receiver, value)
     switch_db_if_test_env
-    result = @con.exec 'INSERT INTO blocks (sender, receiver, value,' \
-                        "previous_tx) VALUES ('#{sender}', '#{receiver}'," \
-                        "'#{value}', '#{Block.prev_tx_hash}') RETURNING *;"
-    Block.new(result.first['id'],
-              result.first['sender'],
-              result.first['receiver'],
-              result.first['value'],
-              result.first['previous_tx'])
+    result = @con.exec 'INSERT INTO blocks (sender, receiver, value, hash, ' \
+            "previous_tx) VALUES ('#{sender}', '#{receiver}', '#{value}', " \
+            "'#{Block.get_hash(sender, receiver, Block.prev_tx_hash)}', " \
+            " '#{Block.prev_tx_hash}') RETURNING *;"
+    Block.new(result.first['id'], result.first['sender'],
+              result.first['receiver'], result.first['value'],
+              result.first['previous_tx'], result.first['hash'])
   end
 
   def self.prev_tx_hash
     switch_db_if_test_env
     block = @con.exec 'SELECT * FROM blocks ORDER BY ID DESC LIMIT 1'
-    block.first['previous_tx']
+    block.first['hash']
+  end
+
+  def self.get_hash(sender, receiver, previous_tx)
+    sha = Digest::SHA256.new
+    sha.update(sender.to_s + receiver.to_s + previous_tx.to_s)
+    sha.hexdigest
   end
 
   private
-
-  def get_hash
-    '0' * 64
-  end
 
   def ==(other)
     @id == other.id
