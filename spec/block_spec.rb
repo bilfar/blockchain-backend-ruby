765189189bf25fbc4450c90c90125fbbcd1a1cf9
Daniel Lau <dct.lau17@gmail.com>
2018-08-24 14:50:23 +0100

    MODEL Add transactions array to block class

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -4,29 +4,8 @@ require 'block'
 require_relative '../lib/helpers'
 
 describe Block do
-  describe '.all' do
-    it 'returns an array of blocks in database' do
-      expect(Block.all.length).to eq 1
-    end
-  end
-
-  describe '.create' do
-    it 'adds a new block instance to the database' do
-      block = Block.create('0', '0', 5)
-      expect(Block.all).to include(block)
-    end
-  end
-
-  describe '.previous_transaction' do
-    it 'returns the previous transaction hash' do
-      expect(Block.prev_tx_hash).to eq('0' * 64)
-    end
-  end
-
-  describe '.hash_block' do
-    it 'initializes block with a hash value' do
-      block = Block.create('0', '0', 5)
-      expect(block.instance_variable_get('@hash').length).to eq 64
-    end
-  end
+  subject(:block) { described_class.new}
+  it 'has a transaction array' do
+   expect(block.transactions).to eq([])
+ end
 end
