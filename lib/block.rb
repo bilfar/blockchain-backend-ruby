5ee2b9db326511350173e3cc786cd459decf394b
Daniel Lau <dct.lau17@gmail.com>
2018-08-25 13:29:18 +0100

    OTHER Fix rubocop formatting

--- lib/block.rb
+++ lib/block.rb
@@ -3,14 +3,13 @@
 require_relative './helpers.rb'
 require 'openssl'
 
-
+# block class
 class Block
   attr_reader :transactions, :previous_hash, :timestamp
 
-  def initialize(transactions: transactions, previous_hash: previous_hash, timestamp: Time.now)
-    @transactions= transactions
+  def initialize(transactions:, previous_hash:, timestamp: Time.now)
+    @transactions = transactions
     @previous_hash = previous_hash
     @timestamp = timestamp
   end
-
 end
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -4,18 +4,22 @@ require 'block'
 require_relative '../lib/helpers'
 
 describe Block do
-  let(:mock_previous_hash) {'previous_hash'}
+  let(:mock_previous_hash) { 'previous_hash' }
   let(:time) { '2018-08-24 15:07:53 +0100' }
-  let(:mock_transactions) do  [ {sender:'9876', receiver:'1234', name: "Spice Girls", value: 1 },
-                                {sender:'9999', receiver:'1234', name: "Spice Girls", value: 1 } ]
+  let(:mock_transactions) do
+    [
+      { sender: '9876', receiver: '1234', name: 'Spice Girls', value: 1 },
+      { sender: '9999', receiver: '1234', name: 'Spice Girls', value: 1 }
+    ]
   end
-  subject(:block) do  described_class.new(transactions: mock_transactions,
-                                          previous_hash: mock_previous_hash,
-                                          timestamp: time)
+  subject(:block) do
+    described_class.new(transactions: mock_transactions,
+                        previous_hash: mock_previous_hash,
+                        timestamp: time)
   end
 
   it 'has a transaction array' do
-   expect(block.transactions).to eq(mock_transactions)
+    expect(block.transactions).to eq(mock_transactions)
   end
 
   it 'takes a previous hash as an argument' do
