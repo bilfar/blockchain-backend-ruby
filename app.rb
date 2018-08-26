ce0b18edb7c576a49574eb8a826e8131747ef0aa
Daniel Lau <dct.lau17@gmail.com>
2018-08-26 22:36:28 +0100

    Feature Add mine button to call create mine_block method

--- app.rb
+++ app.rb
@@ -3,15 +3,25 @@
 require 'json'
 require 'sinatra/base'
 require './lib/blockchain'
+require 'sinatra'
+require 'sinatra/flash'
 
 # Understands sending and receiving transaction hashes
 class BlockchainApp < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
   set :blockchain, Blockchain.new
 
   get '/' do
     @blocks = settings.blockchain.blocks
     @transactions = settings.blockchain.unverified_transactions
     erb(:index)
+  end
+
+  post '/mine' do
+    settings.blockchain.mine_block
+    flash[:notice] = 'Blocks mined sucessfully'
+    redirect '/'
   end
 
   post '/blocks/create' do
