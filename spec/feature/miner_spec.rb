3ec859c9dd422ebef4ddba2c1bbfeedb02bc1b89
Fred <fredch87@outlook.com>
2018-08-26 14:55:06 +0100

    Transactions and Blocks properly formatted into tables with table headings

--- spec/feature/miner_spec.rb
+++ spec/feature/miner_spec.rb
@@ -6,7 +6,7 @@ feature 'viewing blocks' do
   scenario 'see all the blocks stored on the blockchain' do
     visit('/')
     expect(page).to have_content('Blocks')
-    expect(page).to have_content('0000')
+    expect(page).to have_content('N/A')
   end
 end
 
